1bcceaff8e3e7210e0a09a743d0cb31b33879114
Durga Prasad <durgaprasadpentaboina2@gmail.com>
2026-06-26 00:28:15 +0530

    Updated LMS project

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/Global.css";
+// import "./styles/Global.css";
 
 
 ReactDOM.createRoot(
